a00fb7b344a1093d4149996ae816dc5a5f7432ed
skipcheru <sammy.cheruiyot@andela.com>
2016-12-08 11:53:56 +0300

    add more tests

--- tests/test_office_room.py
+++ tests/test_office_room.py
@@ -1,14 +1,33 @@
+from app.person import Fellow, Staff
+from app.room import Office
 import unittest
-from app.person import Person, Fellow, Staff
-from app.amity import Amity, Room, Office, LivingSpace
 
-class TestOffice(unittest.TestCase):
-    """docstring for TestOffice."""
+
+class Testoffice(unittest.TestCase):
+    """docstring for Testoffice."""
     def setUp(self):
-        self.office = Office('Lime')
-        self.fellow = Fellow('ian', 'oti', 'male')
-        self.staff = Staff('njira', 'persc', 'female')
+        self.office = Office('Oculus')
+        self.fellow = Fellow('sam', 'cheru', 'male')
+        self.staff = Staff('angie', 'mugo', 'female')
+        self.occupants =[{200: 'fellow'}, {230: 'fellow2'}, {235: 'staff2'}, {133: 'staff1'}]
 
-    def test_office(self):
-        self.assertIsInstance(self.office, Office, 'Object not an instance of Office')
-        
+    def test_allocate_staff(self):
+        self.office.add_occupant( {233: self.staff} )
+        self.assertListEqual([{233: 'obj1'}], self.office.occupants, msg='list same')
+
+    def test_allocate_fellow(self):
+        self.office.add_occupant( {221: self.fellow} )
+        self.assertIsNot([], self.office.occupants, msg='list should not be equal')
+        self.assertListEqual([{221: self.fellow}, {233: self.staff}], self.office.occupants, msg='list same')
+
+    def test_allocate_staff_if_no_vacany(self):
+        self.office.occupants = self.occupants
+        # add staff to a room which is not vacant
+        add_staff = self.office.add_occupant({908: self.fellow})
+        self.assertEqual(add_staff, 'Sorry! The office is Not Vacant')
+
+    def test_reallocated_person_still_exits(self):
+        self.office.occupants = self.occupants
+         # remove fellow from list of occupants
+        self.office.remove_occupant({200: 'fellow'})
+        self.assertTrue(self.office.is_person_an_occupant({200: 'fellow'}), False)
