681afa78ff72abcb29842ac0742520313cfbdb39
skipcheru <sammy.cheruiyot@andela.com>
2016-12-22 15:51:46 +0300

    fixed load state and save state methods

--- app/database.py
+++ app/database.py
@@ -1,15 +1,17 @@
 import sqlite3
-from amity import Amity
+from app.amity import Amity
+import pickle
 
 
 class AmityData(object):
     """docstring for AmityData."""
 
-    def __init__(self, db_name='data'):
+    def __init__(self, db_name=None):
         self.db_name = db_name
+        database = 'data'
 
-        if not isinstance(self.db_name, str):
-            print('Only string allowed')
+        if self.db_name:
+            database = self.db_name
 
         self.conn = sqlite3.connect(self.db_name + '.db')
         self.cursor = self.conn.cursor()
@@ -18,108 +20,50 @@ class AmityData(object):
 
     def create_db(self):
 
-        andelans = 'CREATE TABLE IF NOT EXISTS all_people(' \
-                'id INTEGER PRIMARY KEY AUTOINCREMENT,' \
-                'staffs TEXT,' \
-                'fellows TEXT, ' \
-                'unallocated_living TEXT, ' \
-                'unallocated_offices TEXT);'
+        all_data = 'CREATE TABLE IF NOT EXISTS all_people(' \
+                    'id INTEGER PRIMARY KEY AUTOINCREMENT,' \
+                    'staffs TEXT,' \
+                    'fellows TEXT, ' \
+                    'unallocated_living TEXT, ' \
+                    'unallocated_offices TEXT, ' \
+                    'offices TEXT, ' \
+                    'living_spaces TEXT);'
 
-        offices = 'CREATE TABLE IF NOT EXISTS offices(' \
-                  'id INTEGER PRIMARY KEY AUTOINCREMENT,' \
-                  'occupants TEXT,' \
-                  'room_name TEXT);'
+        self.cursor.execute(all_data)
 
-        male_living_space = 'CREATE TABLE IF NOT EXISTS male_living(' \
-                            'id INTEGER PRIMARY KEY AUTOINCREMENT,' \
-                            'room_name TEXT,' \
-                            'occupants TEXT);'
+    def save_data(self):
 
-        female_living_space = 'CREATE TABLE IF NOT EXISTS female_living(' \
-                              'id INTEGER PRIMARY KEY AUTOINCREMENT,'\
-                              'room_name TEXT,' \
-                              'occupants TEXT);'
+        fellows= pickle.dumps(self.amity.fellows)
+        staffs = pickle.dumps(self.amity.staffs)
+        unallocated_living = pickle.dumps(self.amity.fellows_unallocated_living_space)
+        unallocated_offices = pickle.dumps(self.amity.andelans_unallocated_offices)
+        offices = pickle.dumps(self.amity.offices)
+        living_spaces = pickle.dumps(self.amity.living_spaces)
 
-        self.cursor.execute(andelans)
-        self.cursor.execute(offices)
-        self.cursor.execute(male_living_space)
-        self.cursor.execute(female_living_space)
-
-    # Save all rooms
-    def save_all_rooms(self):
-
-        self.save_room(self.amity.offices, 'offices')
-
-        self.save_room(self.living_spaces['male'], 'male')
-
-        self.save_room(self.living_spaces['female'], 'female')
-
-    # save each room
-    def save_room(self, rooms, room_type):
-        self.rooms = rooms
-        self.room_type = room_type
-
-        table = ''
-
-        if self.room_type == 'offices':
-            table = 'offices'
-
-        elif self.room_type == 'male':
-            table = 'male_living'
-
-        elif self.room_type == 'female':
-            table = 'female_living'
-
-        else:
-            return False
-
-        # Save all rooms with occupants
-        room_details = [(room.name, str([(member.first_name, member.last_name,
-                        member.gender) for member in room.occupants]))
-                        for room in self.rooms]
-
-        query = 'INSERT INTO '+ table + '(room_name, occupants) VALUES (?, ?)'
-
-        self.cursor.executemany(query, room_details)
-        self.conn.commit()
-
-
-    def save_people(self, people):
-        self.people = people
-        fellows = [(fellow.first_name, fellow.last_name, fellow.gender) fellow in self.amity.fellows]
-
-        staffs = [(staff.first_name, staff.last_name, staff.gender) staff in self.amity.staffs]
-
-        fellows_unallocated_living_space = [(fellow.first_name, fellow.last_name, fellow.gender) fellow in self.amity.fellows]
-
-        self.people = [(self.amity.staffs), (self.amity.fellows),
-                (self.amity.fellows_unallocated_living_space),
-                (self.amity.andelans_unallocated_offices)]
 
         query = 'INSERT INTO all_people (fellows, staffs, unallocated_living,\
-                    unallocated_offices) VALUES (?, ?, ?, ?)'
+                    unallocated_offices, offices, living_spaces) VALUES (?, ?, ?, ?, ?, ?)'
 
-        self.cursor.executemany(query, people)
+        self.cursor.execute(query, (fellows, staffs, unallocated_living,
+                        unallocated_offices, offices, living_spaces))
         self.conn.commit()
+        self.conn.close()
 
 
+    def load_data(self):
+        # Fetch all data and convert to lists.
+        query_section = 'SELECT * FROM all_people WHERE id=1'
+        self.cursor.execute(query_section)
+        data = self.cursor.fetchone()
 
-
-amity = Amity()
-amity.create_room(['Nania office', 'Oculus Office', 'Peri livingspace male'])
-
-amity.add_person('Arya', 'Stark', 'female','staff')
-amity.add_person('Ian', 'Oti', 'male', 'fellow', 'Y')
-amity.add_person('njeri', 'mwangi', 'female','staff')
-amity.add_person('kip', 'cheru', 'male', 'fellow', 'Y')
-
-
-print(amity.living_spaces['male'][0].occupants)
-
-da = AmityData('amdata')
-da.save_room(amity.offices, 'offices')
-da.save_room(amity.living_spaces['male'], 'male')
-
-da.save_people([(amity.staffs), (amity.fellows),
-        (amity.fellows_unallocated_living_space),
-        (amity.andelans_unallocated_offices)])
+        if not data:
+            print("\nNo Data available.\n")
+        else:
+            
+            self.amity.fellows = pickle.loads(data[1])
+            self.amity.staffs = pickle.loads(data[2])
+            self.amity.fellows_unallocated_living_space  = pickle.loads(data[3])
+            self.amity.andelans_unallocated_offices  = pickle.loads(data[4])
+            self.amity.offices = pickle.loads(data[5])
+            self.amity.living_spaces = pickle.loads(data[6])
+            self.conn.close()
