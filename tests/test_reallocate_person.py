33349c4a10be8dedc00a7f77f303eed3a49072b0
skipcheru <sammy.cheruiyot@andela.com>
2017-01-02 16:48:08 +0300

    add test for reallocating male fellow to female living space

--- tests/test_reallocate_person.py
+++ tests/test_reallocate_person.py
@@ -3,23 +3,12 @@ from app.room import Office, LivingSpace
 from app.person import Fellow, Staff
 import unittest
 
+
 class TestReallocatePerson(unittest.TestCase):
-    """docstring for TestReallocatePerson."""
+    """Tests for Reallocate Person."""
     def setUp(self):
         self.amity = Amity()
-        office = Office('oculus')
-        office1 = Office('nania')
-
-        self.amity.offices.append(office)
-        self.amity.offices.append(office1)
-
-        living = LivingSpace('peri')
-        living2 = LivingSpace('swift')
-        living3 = LivingSpace('react')
-
-        self.amity.living_spaces['male'].append(living)
-        self.amity.living_spaces['female'].append(living2)
-        self.amity.living_spaces['female'].append(living3)
+        self.amity.create_room(['oculus o', 'nania o', 'peri l male', 'react l female'])
 
         self.staff = Staff('carol', 'radul', 'female')
         self.fellow = Fellow('Sam', 'Kip', 'male')
@@ -37,15 +26,17 @@ class TestReallocatePerson(unittest.TestCase):
         unknown_room = self.amity.reallocate_person('F001', 'room')
         self.assertEqual('The room is not on the system', unknown_room)
 
-    # check if error is raised if person is staff and the room to be allocated is living
+    # check if error is raised if person is staff and
+    # the room to be allocated is living
     def test_reallocate_staff_to_livingspace(self):
         self.amity.staffs.append({'S001': self.staff})
         self.amity.offices[0].occupants.append(self.staff)
 
         reallocate_staff = self.amity.reallocate_person('S001', 'Peri')
-        self.assertEqual('Sorry staff cannot be allocated livingspace', reallocate_staff)
+        error_msg = 'Sorry staff cannot be allocated livingspace'
+        self.assertEqual(error_msg, reallocate_staff)
 
-    # check if the person above is not reallocated to the specified room
+    # check if staff above is not reallocated to the living space
     def test_staff_not_reallocated_to_living_space(self):
 
         self.assertFalse(self.staff in self.amity.living_spaces['male'][0].occupants)
@@ -56,12 +47,25 @@ class TestReallocatePerson(unittest.TestCase):
         self.amity.reallocate_person('F002', 'react')
         self.assertTrue(self.fellow2 in self.amity.living_spaces['female'][0].occupants)
 
-    # check if staff is moved to another office
+    # check if staff is reallocated to another office
     def test_reallocte_staff(self):
         self.amity.reallocate_person('S001', 'oculus')
         self.amity.reallocate_person('S001', 'nania')
-        self.assertFalse(self.staff in self.amity.offices[0].occupants) # Oculus
-        self.assertTrue(self.staff in self.amity.offices[1].occupants) # Nania
-        # check if the room to be reallocated is the same room which the person occupies
+        self.assertFalse(self.staff in self.amity.offices[0].occupants)
+        self.assertTrue(self.staff in self.amity.offices[1].occupants)
+        # check if person is reallocated to same room
         reallocate_again = self.amity.reallocate_person('S001', 'nania')
-        self.assertEqual('Sorry cannot reallocate. Person is an occupant of this room', reallocate_again)
+        self.assertEqual('Sorry Person is already an occupant of this room'
+                         , reallocate_again)
+
+    # check if the male fellow is not reallocated to female living_space and vice versa
+    def test_reallocte_male_and_female_fellows(self):
+        # add male fellow to system
+        self.amity.fellows.append({'F001': self.fellow})
+        self.amity.fellows.append({'F002': self.fellow2})
+        # reallocate male to female living space and vice versa
+        male_error = self.amity.reallocate_person('F002', 'peri')
+        female_error = self.amity.reallocate_person('F001', 'react')
+
+        self.assertFalse(self.fellow in self.amity.living_spaces['female'][0].occupants)
+        self.assertFalse(self.fellow2 in self.amity.living_spaces['male'][0].occupants)
