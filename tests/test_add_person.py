bde5a9aa69f6bc3893dbee14b14234a19094e3ff
skipcheru <sammy.cheruiyot@andela.com>
2017-01-02 16:53:49 +0300

    add more assertions

--- tests/test_add_person.py
+++ tests/test_add_person.py
@@ -3,64 +3,80 @@ from app.room import Office, LivingSpace
 from app.person import Fellow, Staff
 import unittest
 
+
 class TestAddPerson(unittest.TestCase):
-    """docstring for TestAddPerson."""
+    """TestAddPerson Class."""
     def setUp(self):
         self.amity = Amity()
+        self.amity.create_room(['oculus office', 'Java l male', 'React l female'])
         self.fellow1 = Fellow('sam', 'cheru', 'male')
-        self.fellow2 = Fellow('ian', 'oti', 'male')
+        self.fellow2 = Fellow('debbie', 'asila', 'female')
         self.fellow3 = Fellow('jones', 'mbabe', 'male')
-        self.fellow4 = Fellow('amina', 'abdi', 'female')
+        self.fellow4 = Fellow('debbie', 'asila', 'female')
         self.staff1 = Staff('carol', 'radul', 'female')
-
-    #check if error raised if person details arenot strings or is null or incomplete
-    def test_error_raised(self):
-        self.assertRaises(TypeError, self.amity.add_person(), "error should be raised")
-        self.assertRaises(TypeError, self.amity.add_person(1, 3, 'fellow', 'male'), "error should be raised")
+        self.staff2 = Staff('Janet', 'kamau', 'female')
 
     # check if the person added to the system is a fellow
     def test_person_added_is_type_fellow(self):
-        self.amity.add_person('sam', 'cheru', 'fellow', 'male', 'N')
-        self.assertIsInstance(self.amity.fellows[0]['F001'], Fellow)
+        self.amity.add_person('sam', 'cheru', 'male', 'fellow', 'N')
+        self.amity.add_person('debbie', 'asila', 'female', 'fellow', 'Y')
+        fellow = list(self.amity.fellows[0].values())
+        self.assertEqual(self.fellow1, fellow[0])
 
     # check if the person added to the system is a staff
     def test_person_added_is_type_staff(self):
-        self.amity.add_person('carol', 'radul', 'staff', 'female')
-        self.assertIsInstance(self.amity.staffs[0]['S001'], Staff)
+        self.amity.add_person('carol', 'radul', 'female', 'staff')
+        staff = list(self.amity.staffs[0].values())
+        self.assertEqual(self.staff1, staff[0])
+
+    # test if same person is added twice
+    def test_person_added_is_not_added_twice(self):
+        self.amity.add_person('carol', 'radul', 'female', 'staff')
+        self.assertEqual(len(self.amity.staffs), 1)
 
     # check if the person is allocated a office
     def test_allocate_office_to_fellow_and_staff(self):
-        self.amity.create_room('oculus-o')
-        staff = Staff('carol', 'radul', 'female')
-        fellow = Fellow('sam', 'cheru', 'male')
-        self.assertEqual(staff, self.amity.offices[1].occupants[1], 'staff assigned room')
-        self.assertIn(fellow, self.amity.offices[1].occupants[0], 'fellow assigned office')
+        self.amity.add_person('sam', 'cheru', 'male', 'fellow', 'N')
+        self.amity.add_person('debbie', 'asila', 'female', 'fellow', 'Y')
+        self.amity.add_person('carol', 'radul', 'female', 'staff')
+        self.assertEqual(self.fellow1, self.amity.offices[0].occupants[0])
+        self.assertEqual(self.staff1, self.amity.offices[0].occupants[2])
 
-    # check if male fellows are allocated only male livingspaces
-    def test_allocate_male_fellow_male_living_space(self):
-        self.amity.create_room('java-l-m', 'swift-l-f')
-        self.amity.add_person('jones', 'mbabe', 'fellow', 'male', 'Y')
-        same_fellow = Fellow('jones', 'mbabe', 'male')
-        self.assertIn(same_fellow, self.amity.male_living_spaces[0].occupants, 'fellow allocated only male room')
-        self.assertFalse(same_fellow in self.amity.female_living_spaces[0].occupants, 'confirm fellow allocated only male room')
+    # check if fellows are allocated livingspaces
+    def test_allocate_fellow_living_space(self):
+        self.amity.add_person('sam', 'cheru', 'male', 'fellow', 'Y')
+        self.amity.add_person('debbie', 'asila', 'female', 'fellow', 'Y')
+        self.assertEqual(self.fellow1, self.amity.living_spaces['male'][0].occupants[0])
+        self.assertEqual(self.fellow4, self.amity.living_spaces['female'][0].occupants[0])
 
     # test if fellow who does not need accommodation are not allocated living rooms
     def test_allocate_office_only(self):
         self.amity.add_person('ian', 'oti', 'fellow', 'male', 'N')
         fellow_no_living = Fellow('ian', 'oti', 'male')
-        self.assertFalse(fellow_no_living in self.amity.male_living_spaces[0].occupants, 'fellow not allocated livingspace')
+        self.assertFalse(fellow_no_living in self.amity.living_spaces['male'][0].occupants)
 
     # check if the list of fellows has all the fellow added
-    def test_no_of_fellows_added(self):
-        added_fellows = [{'F001': self.fellow1}, {'F002': self.fellow2}]
-        self.assertListEqual(added_fellows,  self.amity.fellows, 'list equal')
+    def test_no_of_fellows_and_staffs_added(self):
+        self.assertEqual(len(self.amity.fellows), 2)
+        self.assertEqual(len(self.amity.staffs), 1)
 
-    # check if the list of staff has all the staff added
-    def test_no_of_staffs_added(self):
-        added_staffs = [{'S001': self.staff1}, {'S002': self.staff2}]
-        self.assertListEqual(added_staffs,  self.amity.staffs, 'list equal')
+    # check if fellow has been moved to unallocated if no living space
+    def test_unallocated_living_space(self):
+        self.amity.add_person('amina', 'abdi', 'female', 'fellow', 'Y')
+        self.amity.add_person('chess', 'dickson', 'male', 'fellow', 'Y')
+        self.amity.add_person('njeri', 'thome', 'female', 'fellow', 'Y')
+        self.amity.add_person('viola', 'jeruto', 'female', 'fellow', 'Y')
+        self.amity.add_person('nchoe', 'soila', 'female', 'fellow', 'Y')
 
-    # check if person has been moved to unallocated if all rooms are full or no room
-    def test_unallocated_people(self):
-        self.amity.add_person('amina', 'abdi', 'fellow', 'female', 'Y')
-        self.assertTrue(self.fellow4 in self.amity.andelans_unallocated_offices, 'fellow in waiting list')
+        fellow = Fellow('nchoe', 'soila', 'female')
+        self.assertTrue(fellow in self.amity.fellows_unallocated_living_space)
+
+    # check if fellow or staff has been moved to unallocated if no office
+    def test_unallocated_offices(self):
+        self.amity.add_person('digo', 'halikan', 'male', 'staff')
+        fellow = Fellow('viola', 'jeruto', 'female')
+        staff = Staff('digo', 'halikan', 'male')
+
+        self.assertTrue(len(self.amity.andelans_unallocated_offices), 2)
+        self.assertTrue(fellow in self.amity.andelans_unallocated_offices)
+        self.assertTrue(staff in self.amity.andelans_unallocated_offices)
